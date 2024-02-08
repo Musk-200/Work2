98fa585fe8122c2e7327255a03c873f59b6b0211
Muskan Kumari <muskankumari0219@gmail.com>
2024-02-08 10:44:22 +0530

    modified3

--- Jira_API_token.py
+++ Jira_API_token.py
@@ -27,6 +27,6 @@ for index, row in df.iterrows():
       }
    }
    )
-   response=requests.post(url,headers=headers,data=payload,auth=("muskan.kumari@ibm.com","ATATT3xFfGF0uFCv02vcXggFqjNvvhAaozymJrShRLW2nrmBnzUZ1gLyl4X1AIq3pD9hRmWPDJogbufjbLSwQCfIVD8MV-DrJtXg4-9M4JqTrIKhPFWxnwNaoBlkNIy7vjR_mAOXE-pLmZvzYVpUCaC1Svt6awnHiSFA4wi0xCqjYnml2zf2Uuk=9121D468"))
+   response=requests.post(url,headers=headers,data=payload,auth=("muskan.kumari@ibm.com","ATATT3xFfGF0Z0MnmIOXuiDwP-LHr2hM0hNBJ2-pCNYc0-we7SNCHbo4H2zmAv3qbfeJav6CRDxBiAvbTBgiOPL_CpSAKaTx0Xig7JkE7JVvBG2pM1pp6K3UOg1RbYvPIglT3aL9d4PnMV45LSqQEY4MYK65Nf8H1MJKUqjdoD5rDQKSlQPXj3U=38CE73EB"))
    data=response.json()
    print(data["id"])
